6c5655cbafe6479a3904b3149c29feabc4a3eb69
mdcacuci <121271058+mdcacuci@users.noreply.github.com>
2023-01-31 14:40:48 -0800

    Update Constants.java

--- Swerve-Demo-master/src/main/java/frc/robot/Constants.java
+++ Swerve-Demo-master/src/main/java/frc/robot/Constants.java
@@ -41,23 +41,23 @@ public final class Constants
      */
     public static final double DRIVETRAIN_WHEELBASE_METERS = .61; // Measure and set wheelbase
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; // Set front left module drive motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5; //  Set front left module steer motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9; //  Set front left steer encoder ID
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 12; // Set front left module drive motor ID
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 10; //  Set front left module steer motor ID
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 2; //  Set front left steer encoder ID
     public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); //  Measure and set front left steer offset
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2; //  Set front right drive motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 6; //  Set front right steer motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10; //  Set front right steer encoder ID
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 9; //  Set front right steer motor ID
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 5; //  Set front right steer encoder ID
     public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); //  Measure and set front right steer offset
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4; //  Set back left drive motor ID
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8; //  Set back left steer motor ID
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 12; //  Set back left steer encoder ID
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 17; //  Set back left drive motor ID
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 18; //  Set back left steer motor ID
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 3; //  Set back left steer encoder ID
     public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); //  Measure and set back left steer offset
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 3; //  Set back right drive motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7; //  Set back right steer motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; //  Set back right steer encoder ID
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 4; //  Set back right drive motor ID
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; //  Set back right steer motor ID
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 4; //  Set back right steer encoder ID
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); //  Measure and set back right steer offset
 }
